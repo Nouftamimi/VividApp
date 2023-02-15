65b828cf67ee7e5390a74efc5a2e09c81868bf60
Nouf Altamimi <nouftamimi1@gmail.com>
2023-02-15 14:31:50 +0300

    add the files to github

---
+++ VividApp/View/ARDrawView/ARDrawView.swift
@@ -0,0 +1,62 @@
+
+
+import ARKit
+import SwiftUI
+//import MultipeerConnectivity
+
+class ARDrawView: ARSCNView {
+	
+	//var multipeerSession: MultipeerSession?
+	var hexColor: String = ""
+	
+	override init(frame: CGRect, options: [String : Any]? = nil) {
+		super.init(frame: frame, options: options)
+		let config = ARWorldTrackingConfiguration()
+		session.run(config)
+		delegate = self
+		
+		
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let point = touches.first?.location(in: self) else { return }
+		addLineAnchor(point: point)
+	}
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let point = touches.first?.location(in: self) else { return }
+		addLineAnchor(point: point)
+	}
+
+	private func addLineAnchor(point: CGPoint) {
+		let point3D = self.unprojectPoint(SCNVector3(point.x, point.y, 0.997))
+		let node = SCNNode()
+		node.position = point3D
+		let anchor = ARAnchor(name: hexColor, transform: node.simdWorldTransform)
+		session.add(anchor: anchor)
+		//sendToMultipeers(anchor)
+	}
+	
+
+	
+	private func createLine(name: String) -> SCNNode {
+		let sphere = SCNSphere(radius: 0.005)
+		sphere.firstMaterial?.diffuse.contents = UIColor.hexStringToUIColor(hex: name)
+		let node = SCNNode(geometry: sphere)
+		return node
+	}
+	
+
+}
+
+extension ARDrawView: ARSCNViewDelegate {
+	func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+		if let name = anchor.name, name.hasPrefix("#") {
+			node.addChildNode(createLine(name: name))
+		}
+	}
+}
---
+++ VividApp/View/VisualEffectView.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+struct VisualEffectView: UIViewRepresentable {
+	var effect: UIVisualEffect?
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
+}
+
+struct VisualEffectView_Previews: PreviewProvider {
+    static var previews: some View {
+		VisualEffectView(effect: UIBlurEffect(style: .dark))
+    }
+}
