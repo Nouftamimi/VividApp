21d69c5d5c4a242f10668dd5ac2dfc360a89ef10
jum-sm <102621779+jum-sm@users.noreply.github.com>
2023-02-15 17:43:24 +0300

    user id , cloudkit
    fghjkl;

---
+++ Vivid/Model/DrawingModel.swift
@@ -0,0 +1,29 @@
+//
+//  DrawingModel.swift
+//  Vivid
+//
+//  Created by Jumanah  on 24/07/1444 AH.
+//  Copyright © 1444 AH Geonseok Lee. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+//
+//struct userModel : Identifiable {
+//    var id: ObjectIdentifier
+//    
+//   
+//    
+//    
+//    
+//    let ID : CKRecord.ID
+//    init (record:  CKRecord){
+//        self.ID = record.recordID
+//        
+//        
+//        
+//    }
+//    
+//    
+//}
+//
