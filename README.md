b471226b10761da0a1f13deedcab9597b200d848
Nouf Altamimi <nouftamimi1@gmail.com>
2023-02-15 15:21:48 +0300

    add the changes
    add the changes

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Vivid Team
-
+# Privacy Policy for Vivid App
+### Introduction:
+This privacy policy explains how we collect, use, store, and disclose your personal information when you use our Vivid App available on the App Store. Our app is designed to allow users to draw in augmented reality using their device's camera. Our privacy policy is written to ensure that you are aware of what information we collect, how we use it, and how you can control it.
+### Information Collection:
+The only information we collect from you is the access to your device's camera. Our app requires access to the camera to function properly, but we do not collect or store any images or videos taken through the camera. We do not collect any personal information such as your name, address, or email address.
+### Use of Information:
+The access to your camera is solely used to enable the AR Drawing experience in our app. We do not share, sell, or rent your personal information to any third-party.
+Storage and Security of Information:
+We take the protection and security of your personal information seriously. All information collected is securely stored on our servers, and we have implemented appropriate measures to protect your information from unauthorized access, alteration, disclosure, or destruction.
+### Changes to Privacy Policy:
+We may change this privacy policy from time to time to reflect changes in the law or our privacy practices. We encourage you to review our privacy policy periodically for any updates or changes.
+### Contact Us:
+If you have any questions or concerns about our privacy policy or the information we collect and use, please contact us at  
+vividapp.ada@gmail.com .
+### Conclusion:
+We are committed to protecting your privacy and providing you with a secure and enjoyable AR Drawing experience. We hope that this privacy policy has helped you understand our privacy practices and how we use the information collected through our app.
 
---
+++ Vivid/View/ContentView.swift
@@ -0,0 +1,67 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var hexColor: String = "#FFFFFF"
+    
+    var body: some View {
+        let arView = ARDrawViewContainer(hexColor: $hexColor)
+        return ZStack {
+                arView
+                VStack {
+                   HStack {
+                        VStack{
+                            Circle()
+                                .frame(width: 62, height: 62)
+                                .cornerRadius(25)
+                                .foregroundColor(Color("Nouf"))
+                                .opacity(0.4)
+                            
+                            
+                                .overlay(Image(systemName: "globe.asia.australia")
+                                    .font(.system(size: 37))
+                                    .foregroundColor(.white))
+                        }.padding()
+                            Spacer()
+                   
+                        
+                    VStack{
+                        
+                        Rectangle()
+                            .frame(width: 55, height: 150)
+                            .cornerRadius(25)
+                            .foregroundColor(Color("Nouf"))
+                            .opacity(0.4)
+                            .overlay(
+                                VStack {
+                                    Image(systemName: "arrow.uturn.backward")
+                                        .font(.system(size: 37))
+                                        .foregroundColor(.white)
+                                    Spacer().frame(height: 30)
+                                    ColorSelectView(hexColor: $hexColor)
+                                    
+                                })
+                            .padding()
+                    }.padding(.top,50)
+                       
+                }
+                    Spacer()
+        
+                    }
+            
+                   
+                    
+        }.ignoresSafeArea()
+       
+            }
+ 
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+		ContentView()
+    }
+}
